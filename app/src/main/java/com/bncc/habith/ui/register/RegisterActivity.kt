7d3d6ef339a69319702db9d6161c36a74b2c04f8
hafizhMo <hafizh.hizb@gmail.com>
2022-02-03 12:15:00 +0700

    validate input not empty

--- app/src/main/java/com/bncc/habith/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/bncc/habith/ui/register/RegisterActivity.kt
@@ -3,9 +3,12 @@ package com.bncc.habith.ui.register
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.widget.doOnTextChanged
 import com.bncc.habith.databinding.ActivityRegisterBinding
 import com.bncc.habith.ui.login.LoginActivity
 import com.bncc.habith.ui.main.MainActivity
+import com.bncc.habith.util.InputHelper
+import com.bncc.habith.util.InputHelper.inputIsEmpty
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
@@ -13,14 +16,45 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         initBinding()
+        setupInput()
 
         binding.buttonRegister.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
-            finishAffinity()
+            if (!inputIsEmpty(binding.editTextUsername,binding.inputLayoutUsername, this)
+                && !inputIsEmpty(binding.editTextEmail, binding.inputLayoutEmail,this)
+                && !inputIsEmpty(binding.editTextPassword, binding.inputLayoutPassword,this)
+                && !inputIsEmpty(binding.editTextPasswordRepeat, binding.inputLayoutPasswordRepeat,this)
+            ) {
+                startActivity(Intent(this, MainActivity::class.java))
+                finishAffinity()
+            }
         }
 
         binding.textLogin.setOnClickListener {
             finish()
+        }
+    }
+
+    private fun setupInput() {
+        with(binding) {
+            editTextUsername.doOnTextChanged { _, _, _, _ ->
+                if (inputLayoutUsername.isErrorEnabled)
+                    inputLayoutUsername.isErrorEnabled = false
+            }
+
+            editTextEmail.doOnTextChanged { _, _, _, _ ->
+                if (inputLayoutEmail.isErrorEnabled)
+                    inputLayoutEmail.isErrorEnabled = false
+            }
+
+            editTextPassword.doOnTextChanged { _, _, _, _ ->
+                if (inputLayoutPassword.isErrorEnabled)
+                    inputLayoutPassword.isErrorEnabled = false
+            }
+
+            editTextPasswordRepeat.doOnTextChanged { _, _, _, _ ->
+                if (inputLayoutPasswordRepeat.isErrorEnabled)
+                    inputLayoutPasswordRepeat.isErrorEnabled = false
+            }
         }
     }
 
